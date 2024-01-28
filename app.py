93a1783240f01bec84b13770af8de3f88d15c495
rafacalera <rafa_calera@hotmail.com>
2024-01-27 22:36:38 -0300

    update app

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from YoutubeVideo import YoutubeVideo
 
 def App():
     link = input("\nEnter the YouTube video URL: ")
-    resolution = input("\nEnter the option: \n\nhighest resolution (1),\nlowest resolution (2)\n or \naudio only (3):\n\n")
-
     youtube_video = YoutubeVideo(link, App)
+
+    resolution = input("\nEnter the option: \n\nhighest resolution (1),\nlowest resolution (2)\n or \naudio only (3):\n\n")
     youtube_video.download(resolution)
 
 App()
@@ -25,4 +25,4 @@ App()
 #         youtubeObject.download("./videos/", youtubeObject.title+".mp4")
 #     except:
 #         print("An error has occurred")
-#     print("Download is completed successfully")
+#     print("Download is completed successfully")4
